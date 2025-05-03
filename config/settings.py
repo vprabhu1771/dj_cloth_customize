d2aba667de76fc2fc5066341de0a9457a99e412c
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2025-05-03 23:20:22 +0530

    Django Static Setup

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,6 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Optional but recommended
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # for global static files (e.g., CSS/JS shared across apps)
+]
+
+# For production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # collectstatic will copy files here
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
