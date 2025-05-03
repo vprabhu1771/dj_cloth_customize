1408b327cf011e7bbd48027fcdab98edc6f4fa3a
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2025-05-03 23:09:24 +0530

    Category Admin added

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from backend.models import Category
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
