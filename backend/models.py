b43bb6e491ecd3d5608758b6512350cf23612739
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2025-05-04 17:55:13 +0530

    Order, OrderItem model added

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 from django.utils.translation import gettext_lazy as _
@@ -129,3 +131,73 @@ class Cart(models.Model):
 
     class Meta:
         db_table = 'cart'
+
+# Order
+class OrderStatus(models.TextChoices):
+    PENDING = 'PENDING', _('Pending')
+    CONFIRMED = 'CONFIRMED', _('Confirmed')
+    DELIVERY = 'DELIVERY', _('Delivery')
+
+class PaymentMethodStatus(models.TextChoices):
+    CASH = 'CASH', _('CASH')
+    UPI = 'UPI', _('UPI')
+    CARD = 'CARD', _('ON_BACK_ORDER')
+
+class Order(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    customer = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, blank=True, null=True)
+    order_number = models.CharField(max_length=20, unique=True, blank=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    order_status = models.CharField(
+        max_length=255,
+        choices=OrderStatus.choices,
+        default=OrderStatus.PENDING
+    )
+    payment_method = models.CharField(
+        max_length=255,
+        choices=PaymentMethodStatus.choices,
+        default=PaymentMethodStatus.CASH
+    )
+
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self.generate_order_number()
+        super().save(*args, **kwargs)
+
+    def generate_order_number(self):
+        today = datetime.date.today().strftime('%Y-%m-%d')
+        last_order = Order.objects.filter(order_date__date=today).order_by('-id').first()
+        if last_order:
+            last_number = int(last_order.order_number.split()[-1])
+            next_number = str(last_number + 1).zfill(3)
+        else:
+            next_number = '001'
+        return f"{today} {next_number}"
+
+    def __str__(self):
+        return f"{self.order_date.strftime('%d-%m-%Y %H:%M:%S')} {self.customer} {self.total_amount}"
+
+    class Meta:
+        db_table = 'order'
+
+# OrderItem
+class OrderItem(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    # order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    # order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    qty = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.IntegerField()
+
+    # item_total = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.order} {self.product.name} {self.qty} {self.unit_price} {self.amount}"
+
+    class Meta:
+        db_table = 'orderitem'
