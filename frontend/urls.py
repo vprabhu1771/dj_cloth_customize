96eb507f5716e8b80d85d0f9c03a5bcf25e29fc3
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2025-05-04 13:05:04 +0530

    cart urls.py update

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from frontend.views import home, auth_login, auth_logout, register, cart
+from frontend.views import home, auth_login, auth_logout, register, cart, add_to_cart, increase_quantity, \
+    decrease_quantity, remove_from_cart, clear_cart
 
 urlpatterns = [
     path('', home, name="home"),
     path('login', auth_login, name='login'),
     path('logout/', auth_logout, name='logout'),
     path('register', register, name='register'),
-    path('cart', cart)
+    path('cart', cart, name='cart'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/increase/<int:id>/', increase_quantity, name='increase_quantity'),
+    path('cart/decrease/<int:id>/', decrease_quantity, name='decrease_quantity'),
+    path('cart/remove/<int:id>/', remove_from_cart, name='remove_from_cart'),
+    path('cart/clear/', clear_cart, name='clear_cart'),
 ]
